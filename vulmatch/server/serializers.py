859774c2820eb8108ca23d24ceef1d98dc11b820
Fadl <chaos@efqr.dev>
2024-09-26 13:33:18 +0100

    add dates validation #10

--- vulmatch/server/serializers.py
+++ vulmatch/server/serializers.py
@@ -1,5 +1,5 @@
 from .models import Job
-from rest_framework import serializers
+from rest_framework import serializers, validators
 from arango_cti_processor.config import MODE_COLLECTION_MAP
 
 
@@ -15,6 +15,11 @@ class JobSerializer(serializers.ModelSerializer):
 class NVDTaskSerializer(serializers.Serializer):
     last_modified_earliest = serializers.DateField(help_text="(`YYYY-MM-DD`): earliest date")
     last_modified_latest = serializers.DateField(help_text="(`YYYY-MM-DD`): latest date \n* default is `1980-01-01`")
+
+    def validate(self, attrs):
+        if attrs.get('last_modified_earliest') and attrs.get('last_modified_latest') and attrs['last_modified_earliest'] > attrs['last_modified_latest']:
+            raise serializers.ValidationError(f'last_modified_earliest cannot be greater than last_modified_latest')
+        return super().validate(attrs)
 
 class MitreTaskSerializer(serializers.Serializer):
     version = serializers.CharField(help_text="mitre version passed to the script")
