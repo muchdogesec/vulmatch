832762e9177d4884f84a33b29a9142183ad449f8
David G <himynamesdave@users.noreply.github.com>
2025-02-21 07:08:18 +0000

    Update settings.py

--- vulmatch/settings.py
+++ vulmatch/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'dogesec_commons.objects.app.ArangoObjectsViewApp',
+    'dogesec_commons.objects',
     'drf_spectacular',
     'django.contrib.postgres',
     'vulmatch.server',
