9bb9a970f5f73a31a30e893331bf956853b8c94d
Fadl <chaos@efqr.dev>
2025-07-30 17:12:40 +0100

    use workaround for index bug #150

--- tests/full/utils.py
+++ tests/full/utils.py
@@ -1,11 +1,5 @@
 from itertools import tee
 from operator import lt
-import os
-import random
-import time
-from types import SimpleNamespace
-import unittest, pytest
-from urllib.parse import urljoin
 
 def is_sorted(iterable, key=None, reverse=False):
     it = iterable if (key is None) else map(key, iterable)
