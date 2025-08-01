5947d4b2b341c016e37aa2b458180fad8ff97906
Chaos Pjeles <chaos@efqr.dev>
2025-08-01 14:34:14 +0100

    add healthcheck endpoint (#179)
    * add healthcheck endpoint #176
    
    * test new healthcheck/service endpoint
    
    * test new healthcheck/service endpoint ii

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,12 @@
+import pytest
 from vulmatch.worker.populate_dbs import setup_arangodb
 
 def pytest_sessionstart():
     setup_arangodb()
+
+
+@pytest.fixture(scope='session')
+def api_schema():
+    import schemathesis
+    from vulmatch.asgi import application
+    yield schemathesis.openapi.from_asgi("/api/schema/?format=json", application)
