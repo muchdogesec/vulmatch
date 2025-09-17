6d3dcb46bef6889ff38d888ad447e30f09b4b850
Fadl <chaos@efqr.dev>
2025-09-17 14:39:25 +0100

    fix navigator schema

--- vulmatch/server/serializers.py
+++ vulmatch/server/serializers.py
@@ -164,6 +164,7 @@ class AttackNavigatorSerializer(JSONSchemaSerializer):
                         "color": {"type": "string", "pattern": "^#[0-9A-Fa-f]{6}$"},
                         "comment": {"type": "string"},
                         "enabled": {"type": "boolean"},
+                        "showSubTechnique": {"type": "boolean"},
                         "links": {
                             "type": "array",
                             "items": {
