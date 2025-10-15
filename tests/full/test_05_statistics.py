0d4b32960fdd7e99cccc0b140713d0a14d4a48d7
Fadl <chaos@efqr.dev>
2025-10-15 13:53:12 +0100

    fix test failure

--- tests/full/test_05_statistics.py
+++ tests/full/test_05_statistics.py
@@ -79,7 +79,6 @@ def test_kev_stats(client, api_schema, patched_helper):
     api_schema["/api/v1/kev/statistics/"]["GET"].validate_response(
         Transport.get_st_response(resp)
     )
-    print(data)
     assert data == {
         "created_since": {"d1": 4, "d7": 5, "d30": 6, "d365": 10},
         "by_year": [{"year": "2025", "count": 5}, {"year": "2024", "count": 11}],
@@ -116,10 +115,9 @@ def test_cve_stats(client, api_schema, patched_helper):
     api_schema["/api/v1/cve/statistics/"]["GET"].validate_response(
         Transport.get_st_response(resp)
     )
-    print(data)
     assert data == {
         "total_count": 567,
-        "modified_since": {"d1": 412, "d7": 484, "d30": 485, "d365": 567},
+        "modified_since": {"d1": 446, "d7": 484, "d30": 485, "d365": 567},
         "created_since": {"d1": 103, "d7": 438, "d30": 438, "d365": 567},
         "by_year": [{"year": "2024", "count": 567}],
         "cvss_v2": [
@@ -134,20 +132,20 @@ def test_cve_stats(client, api_schema, patched_helper):
             {"range_group": "7.5 -  7.6", "count": 7},
         ],
         "cvss_v3": [
-            {"range_group": "undefined", "count": 64},
+            {"range_group": "undefined", "count": 30},
             {"range_group": "2.4 -  2.5", "count": 1},
             {"range_group": "3.3 -  3.4", "count": 1},
             {"range_group": "3.5 -  3.6", "count": 3},
             {"range_group": "4.0 -  4.1", "count": 1},
             {"range_group": "4.3 -  4.4", "count": 16},
-            {"range_group": "4.6 -  4.7", "count": 2},
-            {"range_group": "4.7 -  4.8", "count": 6},
+            {"range_group": "4.6 -  4.7", "count": 1},
+            {"range_group": "4.7 -  4.8", "count": 7},
             {"range_group": "4.8 -  4.9", "count": 7},
             {"range_group": "4.9 -  5.0", "count": 1},
             {"range_group": "5.2 -  5.3", "count": 2},
             {"range_group": "5.3 -  5.4", "count": 22},
-            {"range_group": "5.4 -  5.5", "count": 23},
-            {"range_group": "5.5 -  5.6", "count": 120},
+            {"range_group": "5.4 -  5.5", "count": 24},
+            {"range_group": "5.5 -  5.6", "count": 147},
             {"range_group": "5.9 -  6.0", "count": 5},
             {"range_group": "6.0 -  6.1", "count": 1},
             {"range_group": "6.1 -  6.2", "count": 13},
@@ -162,7 +160,7 @@ def test_cve_stats(client, api_schema, patched_helper):
             {"range_group": "7.4 -  7.5", "count": 1},
             {"range_group": "7.5 -  7.6", "count": 39},
             {"range_group": "7.6 -  7.7", "count": 2},
-            {"range_group": "7.8 -  7.9", "count": 85},
+            {"range_group": "7.8 -  7.9", "count": 91},
             {"range_group": "8.0 -  8.1", "count": 1},
             {"range_group": "8.1 -  8.2", "count": 2},
             {"range_group": "8.2 -  8.3", "count": 2},
